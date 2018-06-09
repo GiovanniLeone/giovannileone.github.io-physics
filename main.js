b729c04220462fc5f7e1b23facf379d0b148529c
Giovanni Leone <gdleone117@gmail.com>
2018-06-09 17:07:08 -0400

    add slide downs to learn more buttons and restlyed

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+$('.einstein .learn').click(function() {
+  $('.einstein .info').slideToggle(225);
+
+});
+
+$('.curie .learn').click(function() {
+  $('.curie .info').slideToggle(225);
+
+});
+
+$('.bohr .learn').click(function() {
+  $('.bohr .info').slideToggle(225);
+
+});
+
+$('.hawking .learn').click(function() {
+  $('.hawking .info').slideToggle(225);
+
+});
+
+
+});
